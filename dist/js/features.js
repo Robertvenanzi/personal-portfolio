8786cb51d438ba8d694dcbba07eb285fa6499c0b
robertvenanzi <robertgvenanzi@gmail.com>
2018-11-30 23:55:25 -0800

    fixed navbar and menu bug

--- dist/js/features.js
+++ dist/js/features.js
@@ -1,19 +1,5 @@
 
-
-
 $(function () {
-    // Add background color to nav when user scrolls more than 50px down
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 50) {
-            $("#main-navbar").removeClass("navbar-dark");
-            $("#main-navbar").addClass("scrolledNavbar navbar-light");
-        }
-        if ($(this).scrollTop() < 50) {
-            $("#main-navbar").removeClass("scrolledNavbar navbar-light");
-            $("#main-navbar").addClass("navbar-dark");
-        }
-    });
-
     // Enable scrollspy in navbar to show active section of page
     $("body").scrollspy({
         target: "#nav-dropdown",
--- dist/js/main.js
+++ dist/js/main.js
@@ -5,6 +5,11 @@ const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-item');
 
+const nav = document.querySelector('#mainnav');
+const navName = document.querySelector('.nav-name')
+const btnLines = document.querySelectorAll('.btn-line')
+const navIcons = document.querySelectorAll('.nav-icons>a')
+
 // Set intital State of Menu 
 let showMenu = false;
 
@@ -13,11 +18,13 @@ menuNav.addEventListener('click', closeMenu);
 
 function toggleMenu() {
     if (!showMenu) {
+
         menuBtn.classList.add('close');
         menu.classList.add('show');
         menuNav.classList.add('show');
         menuBranding.classList.add('show');
         navItems.forEach(item => item.classList.add('show'));
+
 
         // Set Menu State 
         showMenu = true;
@@ -28,8 +35,11 @@ function toggleMenu() {
         menuBranding.classList.remove('show');
         navItems.forEach(item => item.classList.remove('show'));
 
+
         // Set Menu State 
         showMenu = false;
+
+
     }
 }
 
@@ -42,4 +52,37 @@ function closeMenu() {
     menuBranding.classList.remove('show');
     navItems.forEach(item => item.classList.remove('show'));
 
+    showMenu = false;
+}
+
+// let prevScrollpos = window.pageYOffset;
+// window.onscroll = function () {
+//     let currentScrollPos = window.pageYOffset;
+//     if (prevScrollpos > currentScrollPos) {
+//         document.getElementById("mainnav").style.top = "0";
+//     } else {
+//         document.getElementById("mainnav").style.top = "-55px";
+//     }
+//     prevScrollpos = currentScrollPos;
+// }
+
+window.onscroll = () => {
+
+    if (this.scrollY > 50)
+        nav.classList.add('scrolledNavbar'),
+            navName.style.color = '#000',
+            btnLines.forEach(item => item.style.backgroundColor = '#000'),
+            navIcons.forEach(item => item.style.color = '#000');
+    else
+        nav.classList.remove('scrolledNavbar'),
+            navName.style.color = '#fff',
+            btnLines.forEach(item => item.style.backgroundColor = '#fff'),
+            navIcons.forEach(item => item.style.color = '#fff');
+
+
+
+};
+
+if (showMenu) {
+    btnLines.forEach(item => item.style.backgroundColor = '#fff');
 }
